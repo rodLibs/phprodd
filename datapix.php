499f376c2e9a02b7df449d2f80c8d54dfefe859c
rodrigo <nascimentorodrigo120@gmail.com>
2021-11-23 09:15:41 -0300

    ajustes no json

--- datapix.php
+++ datapix.php
@@ -1,7 +1,8 @@
 <?php
 $json_response = array(
     'qrcode' => '00020126630014br.gov.bcb.pix0111819974848040226Pagamento do pedido 1234565204000053039865406100.005802BR5927jonas airam da silva bonifa6006RECIFE62120508WDEV12346304B556',
-    'qrcode64' => 'em andamento'
+    'qrcode64' => 'em andamento',
+    'txid' => '123txidMocado456'
 );
 
 echo stripslashes(json_encode($json_response)); 
